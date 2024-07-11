c22596c51c9dc5083a11a37cf524eabdfcc303ce
jacksonjjones <jacksonjjones1@gmail.com>
2024-07-11 15:28:05 -0500

    implemented getComputerChoice and getHumanChoice functions

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-console.log("hello, world");
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
 
-function getComputerChoice() {}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  const validChoices = ["rock", "paper", "scissors"];
+  const humanChoice = prompt("please enter rock, paper, or scissors");
+
+  if (validChoices.includes(humanChoice)) {
+    return humanChoice;
+  } else {
+    alert("Invalid choice. Please enter rock, paper, or scissors.");
+    return getHumanChoice();
+  }
+}
+
+console.log(getHumanChoice());
