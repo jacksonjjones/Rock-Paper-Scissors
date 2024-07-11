e5ae0d90c8755b6cc08bc9e5c65940b6cf579abd
jacksonjjones <jacksonjjones1@gmail.com>
2024-07-11 15:58:00 -0500

    implented the playRound function. we can now see the players choices, who won, and scores after each round

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+humanScore = 0;
+computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
-
-console.log(getComputerChoice());
 
 function getHumanChoice() {
   const validChoices = ["rock", "paper", "scissors"];
@@ -18,4 +19,33 @@ function getHumanChoice() {
   }
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log("You win!");
+  } else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    computerScore++;
+    console.log("You lose!");
+  } else {
+    console.log("It's a tie!");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Human choice:", humanSelection);
+console.log("Computer choice:", computerSelection);
+
+playRound(humanSelection, computerSelection);
+
+console.log("Human score:", humanScore);
+console.log("Computer score:", computerScore);
